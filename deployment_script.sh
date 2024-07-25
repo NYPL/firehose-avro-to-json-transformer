c84dc5024447b1473deddfc65cf68d6b30276953
fatimarahman <fatimarahman@nypl.org>
2024-07-25 14:56:35 -0500

    Initial update

--- deployment_script.sh
+++ deployment_script.sh
@@ -2,7 +2,8 @@
 
 rm -f -r ./package
 rm -f deployment-package.zip
-pip3.9 install --target ./package -r requirements.txt
+pip3.12 install --no-dependencies --target ./package -r requirements.txt
+pip3.12 install git+https://github.com/NYPL/python-utils.git@qa-v1.2.0 --target ./package
 cd package
 zip -r ../deployment-package.zip .
 cd ..
