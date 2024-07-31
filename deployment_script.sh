125cccdd156879718af93ad3f5b849a9cc4e648e
fatimarahman <fatimarahman@nypl.org>
2024-07-31 14:48:56 -0500

    Requirements updated

--- deployment_script.sh
+++ deployment_script.sh
@@ -2,7 +2,7 @@
 
 rm -f -r ./package
 rm -f deployment-package.zip
-pip3.12 install --no-dependencies --target ./package -r requirements.txt
+pip3.12 install --target ./package -r requirements.txt
 cd package
 zip -r ../deployment-package.zip .
 cd ..
